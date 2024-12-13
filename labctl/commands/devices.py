1c1b46e0267d9924692718c4962ca3a956bc3714
msterhuj <gabin.lanore@gmail.com>
2024-12-13 22:46:38 +0100

    feat: add delete and logout to devices commands

--- labctl/commands/devices.py
+++ labctl/commands/devices.py
@@ -1,13 +1,19 @@
+from datetime import datetime
 from os import getcwd
+from shutil import which
+from subprocess import run, PIPE
 
 import typer
 
 from rich.table import Table
 
 from labctl.core import Config, APIDriver, console
-from labctl.core import cli_ready, wireguard
+from labctl.core import cli_ready
 
 app = typer.Typer()
+
+def parse_datetime(date_str: str) -> str:
+    return datetime.fromisoformat(date_str.replace("Z", "+00:00")).strftime("%Y-%m-%d %H:%M:%S")
 
 @app.command(name="list")
 @cli_ready
@@ -20,7 +26,6 @@ def list_devices():
     table = Table(title=":computer: Devices")
     table.add_column("ID", style="bold")
     table.add_column("Name", style="cyan")
-    table.add_column("GivenName", style="magenta")
     table.add_column("IPv4", style="green")
     table.add_column("Created At", style="blue")
     table.add_column("Expires At", style="red")
@@ -30,12 +35,11 @@ def list_devices():
     for device in devices:
         table.add_row(
             device["id"],
-            device["name"],
             device["givenName"],
             ", ".join(device["ipAddresses"]),
-            device["createdAt"],
-            device["expiry"],
-            device["lastSeen"],
+            parse_datetime(device["createdAt"]),
+            (device["expiry"] if device["expiry"] != "0001-01-01T00:00:00Z" else "Never"),
+            parse_datetime(device["lastSeen"]),
             "Yes" if device["online"] else "No",
         )
     console.print(table)
@@ -49,11 +53,39 @@ def enroll(name: str = typer.Argument(..., help="The device name")):
     # Todo: Check if tailscale cli is installed and Create preauth key with api and call tailscale cli to enroll device
     ...
 
+@app.command(name="logout")
+def logout():
+    """
+    Self logout
+    """
+    bin = which("tailscale")
+    if not bin:
+        console.print("[red]TailScale cli not found in path please install it[/red]")
+        return
+    print("Running tailscale down...")
+    print("Output: " + run([bin, "down"], stdout=PIPE).stdout.decode())
+    print("Running tailscale logout...")
+    print("Output: " + run([bin, "logout"], stdout=PIPE).stdout.decode())
+
+
 @app.command(name="delete")
 @cli_ready
-def quit():
+def delete(
+    name: str = typer.Argument(None, help="The device name (found on the list command)"),
+):
     """
     Self logout or logout specified device
     """
     # Todo : Check if tailscale cli is installed logout user shutdown tailscale and call api to delete device if asked
     config = Config()
+    api_driver = APIDriver()
+
+    # delete http://localhost:8000/devices/admin/localhost-6uxdpldr
+    status = api_driver.delete(f"/devices/{config.username}/{name}").json()
+
+    # rsp "success": true, "msg": "Device deleted" }
+    if status.get("success"):
+        console.print(f"Device {name} has ben removed from the vpn server :fire:")
+    else:
+        console.print(f"Failed to remove device {name}")
+        console.print(f"Reason given by the server : {status.get("msg")}")
